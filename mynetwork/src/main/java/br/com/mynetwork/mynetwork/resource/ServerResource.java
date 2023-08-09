b0257fb7c8470ae00c969602a0ff29be1d319cce
brunolopeschagas <brunolopeschagas@gmail.com>
2023-08-09 09:23:35 -0300

    finalizado

--- mynetwork/src/main/java/br/com/mynetwork/mynetwork/resource/ServerResource.java
+++ mynetwork/src/main/java/br/com/mynetwork/mynetwork/resource/ServerResource.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -30,76 +31,78 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/server")
 @RequiredArgsConstructor
 public class ServerResource {
-    private final ServerServiceImplementation serverService;
+        private final ServerServiceImplementation serverService;
 
-    @GetMapping("/list")
-    public ResponseEntity<Response> getServers() {
-        return ResponseEntity.ok(
-  sa              Response.builder()
-                        .timeStamp(LocalDateTime.now())
-                        .data(Map.of("servers", serverService.list(30)))
-                        .message("Servers retrieved")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build());
-    }
+        @GetMapping("/list")
+        public ResponseEntity<Response> getServers() {
+                return ResponseEntity.ok(
+                                Response.builder()
+                                                .timeStamp(LocalDateTime.now())
+                                                .data(Map.of("servers", serverService.list(30)))
+                                                .message("Servers retrieved")
+                                                .status(HttpStatus.OK)
+                                                .statusCode(HttpStatus.OK.value())
+                                                .build());
+        }
 
-    @GetMapping("/ping/{ipAddress}")
-    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress)
-            throws UnknownHostException, IOException {
-        Server server = serverService.ping(ipAddress);
-        return ResponseEntity.ok(
-                Response.builder()
-                        .timeStamp(LocalDateTime.now())
-                        .data(Map.of("server", server))
-                        .message(server.getStatus() == Status.SERVER_UP ? "Ping Success" : "Ping Failed")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build());
-    }
+        @GetMapping("/ping/{ipAddress}")
+        public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress)
+                        throws UnknownHostException, IOException {
+                Server server = serverService.ping(ipAddress);
+                return ResponseEntity.ok(
+                                Response.builder()
+                                                .timeStamp(LocalDateTime.now())
+                                                .data(Map.of("server", server))
+                                                .message(server.getStatus() == Status.SERVER_UP ? "Ping Success"
+                                                                : "Ping Failed")
+                                                .status(HttpStatus.OK)
+                                                .statusCode(HttpStatus.OK.value())
+                                                .build());
+        }
 
-    @PostMapping("/save")
-    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
-        return ResponseEntity.ok(
-                Response.builder()
-                        .timeStamp(LocalDateTime.now())
-                        .data(Map.of("server", serverService.create(server)))
-                        .message("Server created")
-                        .status(HttpStatus.CREATED)
-                        .statusCode(HttpStatus.CREATED.value())
-                        .build());
-    }
+        @PostMapping("/save")
+        public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+                return ResponseEntity.ok(
+                                Response.builder()
+                                                .timeStamp(LocalDateTime.now())
+                                                .data(Map.of("server", serverService.create(server)))
+                                                .message("Server created")
+                                                .status(HttpStatus.CREATED)
+                                                .statusCode(HttpStatus.CREATED.value())
+                                                .build());
+        }
 
-    @GetMapping("/get/{id}")
-    public ResponseEntity<Response> getServer(@PathVariable("id") Long id)
-            throws UnknownHostException, IOException {
-        Server server = serverService.get(id);
-        return ResponseEntity.ok(
-                Response.builder()
-                        .timeStamp(LocalDateTime.now())
-                        .data(Map.of("server", server))
-                        .message("Server retrieved")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build());
-    }
+        @GetMapping("/get/{id}")
+        public ResponseEntity<Response> getServer(@PathVariable("id") Long id)
+                        throws UnknownHostException, IOException {
+                Server server = serverService.get(id);
+                return ResponseEntity.ok(
+                                Response.builder()
+                                                .timeStamp(LocalDateTime.now())
+                                                .data(Map.of("server", server))
+                                                .message("Server retrieved")
+                                                .status(HttpStatus.OK)
+                                                .statusCode(HttpStatus.OK.value())
+                                                .build());
+        }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id)
-            throws UnknownHostException, IOException {
-        boolean deleted = serverService.delete(id);
-        return ResponseEntity.ok(
-                Response.builder()
-                        .timeStamp(LocalDateTime.now())
-                        .data(Map.of("deleted", deleted))
-                        .message("Server deleted")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build());
-    }
+        @DeleteMapping("/delete/{id}")
+        public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id)
+                        throws UnknownHostException, IOException {
+                boolean deleted = serverService.delete(id);
+                return ResponseEntity.ok(
+                                Response.builder()
+                                                .timeStamp(LocalDateTime.now())
+                                                .data(Map.of("deleted", deleted))
+                                                .message("Server deleted")
+                                                .status(HttpStatus.OK)
+                                                .statusCode(HttpStatus.OK.value())
+                                                .build());
+        }
 
-    @GetMapping(path = "/image/{fileName}", produces = MediaType.IMAGE_PNG_VALUE)
-    public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
-        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "/Downloads/images" + fileName));
-    }
+        @GetMapping(path = "/image/{fileName}", produces = MediaType.IMAGE_PNG_VALUE)
+        public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
+                ClassPathResource imgFile = new ClassPathResource("images/" + fileName);
+                return Files.readAllBytes(Paths.get(imgFile.getURI()));
+        }
 }
